1374361f92e927b70dd1a63a91362c2920220ac7
Brunvelop <brunvelop@gmail.com>
2023-04-16 10:45:48 +0200

    turbobot try

--- download.py
+++ download.py
@@ -3,6 +3,8 @@ from diffusers import DiffusionPipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
+    device = 0 if torch.cuda.is_available() else -1
+    print("device:",device)
     DiffusionPipeline.from_pretrained(
         "nitrosocke/Future-Diffusion",
         torch_dtype=torch.float32
